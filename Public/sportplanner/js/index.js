82f9fc3a2cc2493ba23b50b70528ef011c0b33fe
ep-friedel <friedel@epages.com>
2017-07-05 01:47:57 +0000

    testing react

--- Public/sportplanner/js/index.js
+++ Public/sportplanner/js/index.js
@@ -1,49 +1,370 @@
 class exercise extends React.Component {
     constructor(props) {
+        super();
+        let i,
+            pastResults = props.pastResults;
+
         this.state =  {
             repetitions: 3,
             completed: false,
-            setup: {
-                'Sitz' : 3,
-                'Lehne' : 5
+            newResults: [],
+            currentRep: {
+                repetition: 1,
+                reps: pastResults[pastResults.length - 1][0].reps,
+                weight: pastResults[pastResults.length - 1][0].weight
             }
         }
 
-    }
-
-    renderSetup() {
-        let propertyname,
-            setup = "";
-
-        for (propertyname in this.props.setup) {
-            setup += this.renderProperty(propertyname, this.props.setup[propertyname]);
+        for (i = 0; i < this.state.repetitions; i++) {
+            this.state.newResults.push({reps: undefined, weight: undefined});
         }
-
-        return (
-            <ul className="setup">
-                {setup}
-            </ul>
-        );
     }
 
-    renderProperty(name, value) {
-        return <li>{name}: {value}</li>;
+    changedReps(newNumber) {
+        let changedRep = Object.assign({}, this.state.currentRep, {reps: parseFloat(newNumber)});
+
+        this.setState({
+            currentRep: changedRep
+        });
+    }
+
+    changedWeight(newNumber) {
+        let changedRep = Object.assign({}, this.state.currentRep, {weight: parseFloat(newNumber)});
+
+        this.setState({
+            currentRep: changedRep
+        });
+    }
+
+    enterResultsHandler() {
+        let s = this.state,
+            cRep = s.currentRep,
+            rep = cRep.repetition,
+            changedResult = s.newResults.slice();
+
+        changedResult[rep - 1] = Object.assign({}, s.newResults[rep - 1], {reps: cRep.reps, weight: cRep.weight})
+
+        if (s.repetitions > rep) {
+            this.setState({
+                newResults: changedResult,
+                currentRep: Object.assign({}, cRep, {repetition: rep + 1, reps: this.props.pastResults[this.props.pastResults.length - 1][rep].reps, weight: this.props.pastResults[this.props.pastResults.length - 1][rep].weight})
+            });
+        } else {
+            this.setState({
+                newResults: changedResult,
+                completed: true
+            });
+        }
     }
 
     render() {
-        return (
-            <div className="exercise{this.state.completed ? ' completed' : ''}">
-                <div class="row">
-                    <img class="exerciseImage" src="this.props.exerciseImageUrl" />
-                    <setup />
-                </div>
-
-            </div>
+        return React.createElement(
+            'div',
+            {className: "exercise" + (this.state.completed ? ' completed' : '')},
+            React.createElement(
+                'div',
+                {className: "row"},
+                React.createElement(
+                    'div',
+                    null,
+                    React.createElement(
+                        'img',
+                        {
+                            className: 'exerciseImage',
+                            src: this.props.details.imageUrl
+                        }
+                    )
+                ),
+                React.createElement(
+                    details,
+                    {details: this.props.details}
+                )
+            ),
+            React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h3',
+                    null,
+                    'Vergangene Trainingseinheiten',
+                ),
+                React.createElement(
+                    'table',
+                    {className: 'pastResults'},
+                    React.createElement(
+                        'tbody',
+                        null,
+                        React.createElement(
+                            resultsRow,
+                            {data: this.props.pastResults[this.props.pastResults.length - 1]}
+                        ),
+                        React.createElement(
+                            resultsRow,
+                            {data: this.props.pastResults[this.props.pastResults.length - 2]}
+                        ),
+                        React.createElement(
+                            resultsRow,
+                            {data: this.props.pastResults[this.props.pastResults.length - 3]}
+                        )
+                    )
+                )
+            ),
+            React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h3',
+                    null,
+                    'Heutige Trainingseinheit',
+                ),
+                React.createElement(
+                    'table',
+                    {className: 'newResults'},
+                    React.createElement(
+                        'tbody',
+                        null,
+                        React.createElement(
+                            resultsRow,
+                            {
+                                data: this.state.newResults
+                            }
+                        )
+                    )
+                ),
+                React.createElement(
+                    enterResults,
+                    {
+                        currentReps: this.state.currentRep.reps,
+                        currentWeight: this.state.currentRep.weight,
+                        changedReps: (newNumber) => this.changedReps(newNumber),
+                        changedWeight: (newNumber) => this.changedWeight(newNumber),
+                        repetition: this.state.currentRep.repetition,
+                        enterResults: () => this.enterResultsHandler(),
+                        completed: this.state.completed
+                    }
+                ),
+                React.createElement(
+                    'button',
+                    {className: (this.state.completed ? 'fullWidthButton' : 'hidden')},
+                    'Nächste Übung wählen'
+                )
+            )
         );
     }
 }
 
-ReactDOM.render(
-  <exercise setup={{'Sitz' : 3, 'Lehne' : 5}} exerciseImageUrl="test123" />,
-  document.getElementById('root')
-);
+
+class details extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        let propertyname,
+            setup = [];
+
+        for (propertyname in this.props.details.setup) {
+            setup.push(this.renderProperty(propertyname, this.props.details.setup[propertyname]));
+        }
+
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h4',
+                null,
+                (this.props.details.machine ? this.props.details.machine + ' - ' : '') + this.props.details.name
+            ),
+            React.createElement(
+                'p',
+                {className: 'note'},
+                this.props.details.note
+            ),
+            React.createElement(
+                'h4',
+                null,
+                'Einstellungen'
+            ),
+            React.createElement(
+                'ul',
+                {className: "setup"},
+                setup
+            )
+        );
+    }
+
+    renderProperty(name, value) {
+        return React.createElement(
+            'li',
+            {key: name},
+            React.createElement(
+                'span',
+                null,
+                name + ':'
+            ),
+            React.createElement(
+                'span',
+                null,
+                value
+            )
+        );
+    }
+}
+
+class resultsRow extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        let rep,
+            cells = [];
+        for (rep in this.props.data) {
+            cells.push(this.renderCell(this.props.data[rep], rep));
+        }
+
+        return React.createElement(
+            'tr',
+            null,
+            cells
+        );
+    }
+
+    renderCell(data, id) {
+        return React.createElement(
+            'td',
+            {key: id},
+            React.createElement(
+                'p',
+                null,
+                (data.reps !== undefined) ? data.reps + ' reps' : ''
+            ),
+            React.createElement(
+                'p',
+                null,
+                (data.weight !== undefined) ? data.weight + 'kg' : ''
+            )
+        );
+    }
+}
+
+class enterResults extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        let rep;
+
+        return React.createElement(
+            'div',
+            {className: 'enterResults' + (this.props.completed ? ' hidden' : '')},
+            React.createElement(
+                'h3',
+                null,
+                this.props.repetition + '. Satz'
+            ),
+            React.createElement(
+                'div',
+                {className: 'row'},
+                React.createElement(
+                    repInput,
+                    {
+                        currentReps: this.props.currentReps,
+                        currentRepsChanged: (newNumber) => this.props.changedReps(newNumber)
+                    }
+                ),
+                React.createElement(
+                    weightInput,
+                    {
+                        currentWeight: this.props.currentWeight,
+                        currentWeightChanged: (newNumber) => this.props.changedWeight(newNumber)
+                    }
+                )
+            ),
+            React.createElement(
+                'button',
+                {className: 'submitResults fullWidthButton', onClick: this.props.enterResults},
+                'Ergebnisse eintragen'
+            )
+        );
+    }
+}
+
+class repInput extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return React.createElement(
+            'div',
+            {className: 'repInput'},
+            React.createElement(
+                'h4',
+                null,
+                'Wiederholungen'
+            ),
+            React.createElement(
+                'input',
+                {
+                    defaultValue: this.props.currentReps,
+                    onChange: (evt) => this.props.currentRepsChanged(evt.target.value)
+                }
+            ),
+        );
+    }
+}
+
+class weightInput extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return React.createElement(
+            'div',
+            {className: 'weightInput'},
+            React.createElement(
+                'h4',
+                null,
+                'Gewicht'
+            ),
+            React.createElement(
+                'input',
+                {
+                    defaultValue: this.props.currentWeight,
+                    onChange: (evt) => this.props.currentWeightChanged(evt.target.value)
+                }
+            ),
+        );
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    ReactDOM.render(
+        React.createElement(
+            exercise,
+            {
+                details: {
+                    imageUrl: "./images/situp.jpg",
+                    machine: 'V18',
+                    name: 'Butterfly',
+                    note: 'Arme nach innen drücken',
+                    setup: {
+                        'Sitz' : 3,
+                        'Lehne' : 5
+                    }
+                },
+                pastResults: [
+                    [{reps: 18, weight: 12},{reps: 20, weight: 12},{reps: 16, weight: 12}],
+                    [{reps: 16, weight: 15},{reps: 16, weight: 15},{reps: 16, weight: 15}],
+                    [{reps: 18, weight: 12},{reps: 20, weight: 12},{reps: 16, weight: 12}],
+                    [{reps: 18, weight: 12},{reps: 20, weight: 12},{reps: 16, weight: 12}]
+                ]
+            }
+        ),
+        document.getElementById('root')
+    );
+});
